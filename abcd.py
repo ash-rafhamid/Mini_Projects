9144b475d17bad4e366f6e60d961efe463fc9714
ashraf hamid <110883341+ash-rafhamid@users.noreply.github.com>
2023-09-24 22:17:45 +0600

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -1,15 +1,26 @@
 import random
 
-lettergit m_letter}: ").strip().capitalize()
+def game():
+    # Define a list of words for the game
+    words = ["apple", "banana", "cherry", "grape", "lemon", "orange", "strawberry"]
 
-        if guess == word:
-            print("Congrats! Your guess is correct.")
-        else:
-            print(f"Sorry, the correct word is '{word}'.")
+    while True:
+        # Choose a random word from the list
+        word = random.choice(words)
+
+        # Initialize an empty string to store the player's guess
+        guess = ""
+
+        while guess != word:
+            guess = input("Guess the word: ").strip().capitalize()
+
+            if guess == word:
+                print("Congrats! Your guess is correct.")
+            else:
+                print(f"Sorry, the correct word is '{word}'.")
 
         play_again = input("Do you want to play again? (yes/no): ").strip().lower()
         if play_again != 'yes':
             break
-
 
 game()
